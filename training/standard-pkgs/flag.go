8c5f7c2e99805c1207cd5bfa29aef6e400376748
vjudge <gradonday@gmail.com>
2021-09-26 14:55:42 +0800

    add sample: flag

---
+++ training/standard-pkgs/flag.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+var name string
+
+func init () {
+	flag.StringVar(&name, "name", "everyone", "The greeting object.")
+}
+
+func main ()  {
+	flag.Parse()
+	fmt.Printf("Hello, %s!\n", name)
+}
+
+// go run flag.go --name vjudge
