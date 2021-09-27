840df1c2c2ec38419129e03bcf6fb668da96926c
vjudge <gradonday@gmail.com>
2021-09-27 22:58:01 +0800

    add map

--- training/language-grammars/map.go
+++ training/language-grammars/map.go
@@ -1,1 +1,19 @@
 package main
+
+import "fmt"
+
+func main ()  {
+	mmap := map[string]int{
+		"first": 1,
+		"second": 2,
+	}
+	fmt.Println(mmap)
+	key := "first"
+	//key := "three"
+	val, ok := mmap[key]
+	if ok {
+		fmt.Printf("The element of key %s: %d", key, val)
+	} else {
+		fmt.Println("not found", ok)
+	}
+}
