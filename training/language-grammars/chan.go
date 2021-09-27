d1ecad51cb6d9cd5d7dda41372761e46ab340aa3
vjudge <gradonday@gmail.com>
2021-09-27 22:57:48 +0800

    add chan

--- training/language-grammars/chan.go
+++ training/language-grammars/chan.go
@@ -1,1 +1,17 @@
 package main
+
+import "fmt"
+
+func main ()  {
+	ch1 := make(chan int, 5)
+	ch1 <- 1
+	ch1 <- 2
+	ch1 <- 3
+	ch1 <- 4
+	ch1 <- 5
+	//ch1 <- 6
+	for i := 1; i <= 6; i++ {
+		ele := <-ch1
+		fmt.Println("ele:", i, ele)
+	}
+}
