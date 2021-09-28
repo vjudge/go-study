e97f2fadfb6e536d07a8a3a4ffd3dcc9c3c347f2
vjudge <gradonday@gmail.com>
2021-09-28 23:10:48 +0800

    update struct

--- training/language-grammars/struct.go
+++ training/language-grammars/struct.go
@@ -1,1 +1,19 @@
 package main
+
+import "fmt"
+
+func main ()  {
+	// 定义
+	type Person struct {
+		Name string
+		Gender string
+		Age uint8
+	}
+	// 初始化
+	p1 := Person{Name: "Robert", Age: 33}
+	fmt.Printf("Name: %s, Gender: %s, Age: %d \n", p1.Name, p1.Gender, p1.Age)
+	// Name: Robert, Gender: , Age: 33
+	p2 := Person{"vjudge", "Female", 18}
+	fmt.Printf("Name: %s, Gender: %s, Age: %d \n", p2.Name, p2.Gender, p2.Age)
+	// Name: vjudge, Gender: Female, Age: 18
+}
