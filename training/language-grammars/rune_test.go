9328f2a8275ff75a7e96c28247337bea6dcbb046
vjudge <gradonday@gmail.com>
2021-10-09 23:35:55 +0800

    add rune

--- training/language-grammars/rune_test.go
+++ training/language-grammars/rune_test.go
@@ -1,1 +1,14 @@
-package main
+package main_test
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRune (t *testing.T) {
+	str := "放假就开始学 Go"
+	fmt.Printf("str: %q\n", str)
+	fmt.Printf("str-char: %q\n", []rune(str)) // char
+	fmt.Printf("str-hex: %x\n", []rune(str)) // hex
+	fmt.Printf("str-hex: [% x]\n", []byte(str)) // hex
+}
