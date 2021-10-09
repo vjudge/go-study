4cc6412bc46a9be602b24b9b273ed752f9c7739c
vjudge <gradonday@gmail.com>
2021-10-09 13:50:54 +0800

    update panic

--- training/language-grammars/panic.go
+++ training/language-grammars/panic.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func main ()  {
+	// 正确捕获 panic
+	fmt.Println("start main")
+	defer func() {
+		fmt.Println("start defer")
+		if p := recover(); p != nil {
+			fmt.Println("panic: ", p)
+		}
+		fmt.Println("end defer")
+	}()
+	panic(errors.New("something is wrong"))
+	fmt.Println("end main")
+	//// 依然会异常退出
+	//fmt.Println("start main")
+	//panic(errors.New("something is wrong"))
+	//p := recover()
+	//fmt.Println("panic: ", p)
+	//fmt.Println("end main")
+}
