f04dbe3026834f180976006800409b13c75712f3
vjudge <gradonday@gmail.com>
2021-10-09 13:50:26 +0800

    add error

--- training/language-grammars/error.go
+++ training/language-grammars/error.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+func print(req string) (res string, err error) {
+	if req == "" {
+		err = errors.New("req is empty")
+		return
+	}
+	fmt.Println("req: ", req)
+	return
+}
+
+func main ()  {
+	_, err1 := print("vjudge coming")
+	fmt.Println("err:", err1)
+	_, err2 := print("")
+	fmt.Println("err:", err2)
+	os.Exit(0)
+}
